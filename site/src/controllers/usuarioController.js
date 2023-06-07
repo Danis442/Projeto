1d941627f6b63f836595d98006052fdb9877cc77
Danis442 <danilo.faustino@sptech.school>
2023-06-07 14:49:16 -0300

    Grafico plotando

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -93,10 +93,43 @@ function cadastrar(req, res) {
             );
     }
 }
+function favoritos(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var favoritos = req.body.favoritoServer;
+    var id = req.body.idServer;
+
+    // Faça as validações dos valores
+    if (favoritos == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    
+    }else if(id == undefined){
+        res.status(400).send("Seu id está undefined!");
+    } else {
+        
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.favoritos(favoritos, id)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
 
 module.exports = {
     entrar,
     cadastrar,
+    favoritos,
     listar,
     testar
 }
